bcef82dbf72c1f9d73e8eae8723851bf65e4eb0a
Alain Hélaïli <alain@carbonhub.io>
2020-06-24 16:08:10 +0200

    using axios

--- src/main.js
+++ src/main.js
@@ -2,16 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+const httpclient = axios.create({
+  baseURL: 'https://api.github.com/octocat',
+  timeout: 1000,
+  headers: {'Authorization': `token ${process.env.TOKEN}`}
+})
+
+Vue.prototype.$http = httpclient
 
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
-
-
-function endsWith(x, y) {
-  return x.lastIndexOf(y) === x.length - y.length;
-}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png"  @click="clicked()">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -13,6 +13,14 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  methods: {
+    clicked () {
+      console.log('clicked')
+    },
+    endsWith (x, y) {
+      return x.lastIndexOf(y) === x.length - y.length
+    }
   }
 }
 </script>
